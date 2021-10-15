0707617f8948f075334273fea5d71eb3336dfba3
Morre Thierry <Briareos@live.be>
2021-10-15 16:07:42 +0200

    Add Secure ConnectionString

--- ISOSL_GestContact.ViewModels/ViewModelLocator.cs
+++ ISOSL_GestContact.ViewModels/ViewModelLocator.cs
@@ -5,6 +5,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -12,6 +13,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using ToolBox.Cryptography;
 using Tools.Connection.Database;
 using Tools.Patterns.Locator;
 using Tools.Patterns.Mediator;
@@ -29,6 +31,10 @@ namespace ISOSL_GestContact.ViewModels
 
         protected override void ConfigureServices(IServiceCollection services)
         {
+            ICryptoRSA crypto = new CryptoRSA(Properties.Resources.Keys);
+            ConnectionInfo connectionInfo = JsonConvert.DeserializeObject<ConnectionInfo>(crypto.Decrypter(Properties.Resources.ConnectionInfo));
+            string connectionString = string.Format(Configuration.GetConnectionString("ISOSL_GestContact"), connectionInfo.Login, connectionInfo.Passwd);
+
             services
 #if DEBUG
                 .AddLogging(c =>
@@ -40,7 +46,7 @@ namespace ISOSL_GestContact.ViewModels
 #endif
                 .AddDispatcher()
                 .AddViews()
-                .AddSingleton<IConnection>(sp => new Connection(SqlClientFactory.Instance, Configuration.GetConnectionString("ISOSL_GestContact")))
+                .AddSingleton<IConnection>(sp => new Connection(SqlClientFactory.Instance, connectionString))
                 .AddSingleton<IMessenger<PersonneViewModelMessage>, Messenger<PersonneViewModelMessage>>()
                 .AddSingleton<IMessenger<OpenWindowMessage<PersonneViewModel>>, Messenger<OpenWindowMessage<PersonneViewModel>>>()
                 .AddSingleton<IPersonneRepository, PersonneService>()
@@ -57,6 +63,12 @@ namespace ISOSL_GestContact.ViewModels
         public PaysViewModel Pays
         {
             get { return Container.GetService<PaysViewModel>(); }
+        }
+
+        private class ConnectionInfo
+        {
+            public string Login { get; set; }
+            public string Passwd { get; set; }
         }
     }
 }
---
+++ Tools/Cryptography/CryptoRSA.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ToolBox.Cryptography
+{
+    public class CryptoRSA : ICryptoRSA
+    {
+        private RSACryptoServiceProvider _RSACSP;
+        private UTF8Encoding _Converter;
+
+        public string XmlPublicKey
+        {
+            get { return _RSACSP.ToXmlString(false); }
+        }
+
+        public string XmlBothKeys
+        {
+            get { return _RSACSP.ToXmlString(true); }
+        }
+
+        public byte[] BinaryPublicKey
+        {
+            get { return _RSACSP.ExportCspBlob(false); }
+        }
+
+        public byte[] BinaryBothKeys
+        {
+            get { return _RSACSP.ExportCspBlob(true); }
+        }
+
+        public CryptoRSA() : this(KeySizes.RSA2048)
+        {
+
+        }
+
+        public CryptoRSA(KeySizes KeySize)
+        {
+            _RSACSP = new RSACryptoServiceProvider((int)KeySize);
+            _Converter = new UTF8Encoding();
+        }
+
+        public CryptoRSA(string XmlKeys)
+        {
+            _RSACSP = new RSACryptoServiceProvider();
+            _Converter = new UTF8Encoding();
+
+            if (XmlKeys != null)
+            {
+                _RSACSP.FromXmlString(XmlKeys);
+            }
+        }
+
+        public CryptoRSA(byte[] BinaryKeys)
+        {
+            _RSACSP = new RSACryptoServiceProvider();
+            _Converter = new UTF8Encoding();
+
+            if (BinaryKeys != null)
+            {
+                _RSACSP.ImportCspBlob(BinaryKeys);
+            }
+        }
+
+        public byte[] Crypter(string ACrypter)
+        {
+            if (ACrypter.Length > ((_RSACSP.KeySize / 8) - 42) / 2)
+                throw new InvalidOperationException(string.Format("Chaine trop longue!!! (Taille Maximale : {0})", ((_RSACSP.KeySize / 8) - 42) / 2));
+
+            byte[] ACrypterEnByte = _Converter.GetBytes(ACrypter);
+            return _RSACSP.Encrypt(ACrypterEnByte, true);
+        }
+
+        public string Decrypter(byte[] ADecrypter)
+        {
+            if (_RSACSP.PublicOnly)
+            {
+                throw new InvalidOperationException("Pour décrypter il vous faut la clé privée!!");
+            }
+
+            byte[] Decrypte = _RSACSP.Decrypt(ADecrypter, true);
+            return _Converter.GetString(Decrypte);
+        }
+
+        public void ImportBinaryKeys(byte[] keys)
+        {
+            _RSACSP.ImportCspBlob(keys);
+        }
+
+        public void ImportXmlKeys(string xml)
+        {
+            _RSACSP.FromXmlString(xml);
+        }
+    }
+}
